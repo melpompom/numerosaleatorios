979d681ba179e1e91dac46687ba51b41efe33074
Melissa <91746003+melissatonon@users.noreply.github.com>
2021-10-21 23:11:52 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,67 +1,82 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+<?php
+    $vezes = @$_POST['vezes'];
+?>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Par ou impar</title>
+	<title>Jogo de Dado</title>
 </head>
 <body>
+<form method="post"><b>
+Jogar <input type="text" name="vezes" vezes o dado.>
+<input type="hidden" name="jogada" value="1">
+<input type="submit" value="JOGAR">
+</form>
 
 <?php
-            $impar = 0;
-            $par = 0;
-            $resultado = "--";
-            $soma = 0;
 
-            if(isset($_POST["geranum"])){
-                $impar = rand(0, 10);
-                $par = rand(0, 10);
-                $soma = $impar+$par;
+   if (! isset($_POST['jogada'])){
+   $cont = 1; 
+   }
 
-                if($soma % 2 == 0){
-                    $resultado = "par";
-                }else{
-                    $resultado = "impar";
-                }
-            }
-        ?>
+   if (! isset($cont)){
 
-<div>
-<h1>Jogo Par ou impar</h1><br>
+   if (empty($_POST['vezes'])){
+   $cont2 = 1;
+   }
+   
+   if (! isset($cont2)){
 
-<div>
-                    <label>Escolha um número!</label>
-                    <select>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                    </select>
-                    <br>
-<label>Informe se é par ou impar:</label><br>
-<input type="radio" name="nome" value="Par">Par<br>
-<input type="radio" name="nome" value="Impar">Impar<br>
-</div>
-<br>
+   if ($_POST['vezes'] < 1){
+   $cont3 = 1;
+   }
+ 
+   if (! isset($cont3)){   
 
-<form method="POST">
-                <input type="submit" name="geranum" value="Enviar!">
-            </form>
-<div> 
-<br>
+   for ($i = 0; $i < $vezes; $i ++){
+   $jogador[] = mt_rand(1, 6); 
+   }
+   
+   $tjogador = 0;
 
-</div>
-            <div class ="resultado">
-                <p>Resultado: <?php echo $resultado;?></p>
-            </div>
+   for($i = 0; $i < $vezes; $i++){
+   $vc = $jogador[$i];
+  
+  switch($vc)
+   {
+      case 1 :
+      echo "\n<img src='1.gif' border='0'>";
+      break;
+      case 2 :
+      echo "\n<img src='2.gif' border='0'>";
+      break;
+      case 3 :
+      echo "\n<img src='3.gif' border='0'>";
+      break;
+      case 4 :
+      echo "\n<img src='4.gif' border='0'>";
+      break;
+      case 5 :
+      echo "\n<img src='5.png' border='0'>";
+      break;
+      case 6 :
+      echo "\n<img src='6.png' border='0'>";
+      break;
+   } 
+   $tjogador += $vc;
+   }
 
-</div>
+   echo "\n<br>\n<font face='verdana' size='2' color='#000080'>\n<b>Total :" . $tjogador . "</b>\n</font>\n<br>\n<br>";
+
+   if ($tjogador <= $vezes*3){
+   echo "<b>Que pena você perdeu.</b>";
+   } else {
+   echo "<b>Você ganhou!</b></";
+   }
+}
+}
+}
+?>
+<br><br><br>
 </body>
 </html>
+
